c86e21889577010bf55c061827c91482dff1c4d9
Ashir Raj <ashirraj08@gmail.com>
2026-08-18 19:25:53 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -43,7 +43,8 @@ app.get("/healthz", (req, res) => {
   });
 });
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
+
+app.listen(PORT, "0.0.0.0", () => {
   connectDB();
   console.log(`Server is running on port ${PORT}`);
 });
